af7ed9f873e7679d2f5ae0ec866c6722df82e7d1
MuhammadHanzala31 <hanzala.sispn@gmail.com>
2025-06-03 03:13:04 +0500

    set cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from 'express'
-
+import cors from 'cors'
 const app = express()
+app.use(cors({
+    origin : ['http://localhost:8081/', "*"]
+}))
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static("public"))
