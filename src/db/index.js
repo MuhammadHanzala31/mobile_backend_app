8da07fa898532615b7bf996bb830c2829276b474
MuhammadHanzala31 <156145091+MuhammadHanzala31@users.noreply.github.com>
2025-06-03 01:04:53 +0500

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 
 const connectDb = async () =>{
     try {
-        const connectionInstance = await mongoose.connect(`mongodb://localhost:27017/mobileApp`)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://hanzalasispn:Pakistan92@cluster0.7nxat.mongodb.net/mobileApp`)
         console.log(`database connected on port :: ${connectionInstance.connection.port}`);
         
     } catch (error) {
